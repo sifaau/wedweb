f33cfe66db5d8f999a260b2b9f2118425fab0b2f
sifaau <fasyifaaus@gmail.com>
2022-06-10 13:47:47 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>The Wedding of Dwi & Miftah</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+	<link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/superfish.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/modernizr-2.6.2.min.js"></script>
+</head>
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+
+		<div class="fh5co-hero" data-section="home">
+			<div class="fh5co-overlay" style="background:transparent;"></div>
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="1" style="background-color: white ;
+			background-repeat: no-repeat;
+ 			background-position: center center !important;
+			background-image: url(images/cover_bg_1.png);">
+				<div class="display-t">
+					<div class="display-tc">
+						<div class="container">
+							<div class="col-md-10 col-md-offset-1">
+								<div class="animate-box" >
+									<h1 style="color:black">The Wedding</h1>
+									<h2 style="color:black">Dwi &amp; Miftah</h2>
+									<p style="color:black"><span style="color:black;border-top:2px solid #3d5736;border-bottom:2px solid #3d5736;">10.06.2022</span></p>
+									<br>
+									<div class="col-md-4 col-md-offset-4">
+									<a href="undangan" class="btn btn-primary btn-block" style="background:#3d5736;">Buka Undangan</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-countdown" style="background:#3d5736;">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center animate-box" style="color:white;">
+					Save the date
+				</div>
+				<div class="col-md-8 col-md-offset-2 text-center animate-box">
+					<p class="countdown" style="">
+						<span id="days"></span>
+						<span id="hours"></span>
+						<span id="minutes"></span>
+						<span id="seconds"></span>
+					</p>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	</div>
+
+	<footer>
+		<div id="footer" style="background:#3d5736;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+					</div>
+					<div class="col-md-6 col-md-offset-3 text-center">
+						<p>Made with <i class="icon-heart3"></i>  by <a target="_blank" href="https://www.instagram.com/fasyifaaus/"><i class="icon-instagram"></i> fasyifaaus </a> <br><small>Rustic floral vector created by mariadetarosarinda @ www.freepik.com</small></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<script src="dist/mss.js"></script>
+
+	<!-- SILAKAN JIKA ADA YANG COPAS SCRIPTNYA. BOLEH AJA GRATIS -->
+
+	</body>
+</html>
---
+++ undangan/index.php
@@ -0,0 +1,386 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>The Wedding of Dwi & Miftah</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+	<link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/superfish.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<style>
+
+	.open-button {
+  /* background-color: #555; */
+  /* color: white; */
+	background-color:transparent;
+  padding: 10px 10px;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+  position: fixed;
+  bottom: 23px;
+  right: 0px;
+  width: 80px;
+}
+</style>
+</head>
+<body>
+	<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+
+			<header id="fh5co-header-section" class="sticky-banner">
+				<div class="container">
+					<div class="nav-header">
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
+						<h1 id="fh5co-logo"><a href="home" style="color:#3d5736;">Undangan</a></h1>
+						<nav id="fh5co-menu-wrap" role="navigation">
+							<ul class="sf-menu" id="fh5co-primary-menu">
+								<li><a href="home" >Home</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
+
+			<div class="fh5co-hero" data-section="home">
+				<div class="fh5co-overlay"></div>
+				<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(images/cover_bg_1.png);">
+					<div class="display-t">
+						<div class="display-tc">
+							<div class="container">
+								<div class="col-md-10 col-md-offset-1">
+									<div class="animate-box">
+										<p>
+											Maha Suci Allah SWT,
+											yang telah menciptakan makhluknya
+											berpasang-pasangan.
+											<br>
+											Ya Allah, semoga RidhoMu tercurah mengiring<bR>
+												pernikahan putra - putri kami :
+											</p>
+											<br>
+											<h2>Dwi &amp; Miftah </h2>
+											<!-- <span style="color:#fff;">scroll down</span><br> --><br>
+											<img src="images/down.png" alt="Girl in a jacket" width="30" height="30">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+				<!-- end:header-top -->
+
+				<div  id="fh5co-cover"  data-stellar-background-ratio="0.5" style="background-image: url(images/cover_bg_3.png);background-size:cover;background-position:bottom;background-repeat: no-repeat;">
+					<div class="container">
+						<div class="row animate-box">
+							<div class="col-md-8 col-md-offset-2 text-center heading-section">
+								<br>
+								<br>
+								<br>
+								<h2 style="color:#3d5736;">Assalamu’alaikum<br>warahmataullahi Wabarakatuh</h2>
+							</div>
+						</div>
+						<div class="row ">
+							<div class="col-md-12 text-center">
+								<br>Kami bermaksud menyelengkarakan acara perhikahan
+								putra - putri kami :<br><br>
+							</div>
+							<div class="col-md-3 text-center animate-box col-md-offset-3">
+								<div class="groom-men">
+									<h2 style="color:#3d5736;">Dwi Nur Adhiyat Ekawati</h2>
+									<p><strong> Putri Bpk. Safrizal Riza<br>& Ibu Neneng Nurhayati ( Almh )<br>Purwakarta</strong></p>
+
+
+								</div>
+							</div>
+							<div class="col-md-3 text-center animate-box">
+								<div class="groom-men">
+									<h2 style="color:#3d5736;">Miftah Syifa'au Shudur</h2>
+									<p><strong>Putra Bpk. Masyhudi<br>& Ibu Siti Amsiyah<br>Purwodadi</strong></p>
+									<br>
+									<br>
+									<br>
+									<br>
+									<br>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+				<div id="fh5co-when-where" class="fh5co-section-gray">
+					<div class="container">
+						<div class="row" style="display:none;">
+							<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+								<h2>When &amp; Where</h2>
+							</div>
+						</div>
+						<div class="row row-bottom-padded-md">
+
+							<div class="col-md-6 text-center animate-box">
+								<div class="wedding-events">
+									<div class="desc">
+										<h3>Akad Nikah</h3>
+										<p><strong>Minggu, 10 Juli 2022, 18.00 WIB</strong></p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 text-center animate-box">
+								<div class="wedding-events">
+									<div class="desc">
+										<h3>Resepsi Nikah</h3>
+										<p><strong>Minggu, 10 Juli 2022, 10.00 - 16.00 WIB</strong></p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<p>Bertempat di : <br><strong>Jalan Baru Gang Cempaka 1 No.20 RT.33/04 <br>Kec. Purwakarta, Kel. Nagri Kaler, Purwakarta
+								</strong></p>
+							</div>
+							<div class="col-md-12">
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d990.9763888426868!2d107.4435833!3d-6.5336111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6b72054b1cc755c3!2zNsKwMzInMDEuMCJTIDEwN8KwMjYnMzYuOSJF!5e0!3m2!1sid!2sid!4v1654522497311!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+							</div>
+							<div class="col-md-12">
+								<a href="https://goo.gl/maps/EUeh5HKHJJ2mWd4EA" class="btn btn-primary btn-block" target="_blank" style="background:#3d5736;">Buka Google Map</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+				<div id="fh5co-cover" style="background:#3d5736;background-image: url(images/cover_bg_4.png);background-size:cover;" data-stellar-background-ratio="0.5">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6 col-md-offset-3 text-center" style="color:white;">
+								<br><br>
+								<!-- Merupakan suatu kehormatan dan kebahagiaan bagi kami
+								apabila Bapak/Ibu/Saudara/i, berkenan hadir
+								untuk memberikan doa restu kepada
+								kedua mempelai. -->
+
+								<!-- Atas kehadiran dan doa restunya kami ucapkan terima kasih.<br><br> -->
+								<!-- <i>Wassalamu’alaikum Warahmatullahi Wabarakatuh.</i> -->
+							</div>
+							<div class="col-md-12 text-center">
+								<br><br><br><br>
+								<h2 style="color:white;">Kami Yang Berbahagia</h2>
+							</div>
+							<div class="col-md-6 col-md-offset-3 text-center" style="color:white;">
+								<p style="color:white;">
+									Keluarga Bapak Safrizal Riza &
+									Ibu Neneng Nurhayati ( Almh )
+								</p>
+								<p style="color:white;">
+									Keluarga Bapak Masyhudi &
+									Ibu Siti Amsiyah
+								</p>
+							</div>
+
+							<div class="col-md-12 text-center">
+								<br><br><br><br>
+								<h2 style="color:white;">Turut Berbahagia</h2>
+							</div>
+							<div class="col-md-6 col-md-offset-3 text-center" style="color:white;">
+								<!-- <hr> -->
+								<p style="color:white;">
+									Keluarga Besar Soemawinata<br>
+									Keluarga Besar Abdul Qodir<br>
+									Keluarga Besar Moh Kerin<br>
+									Keluarga Besar Hasan Marja<br>
+									<!-- PT. Sanwa Musen Ind -->
+								</p>
+								<br><br><br><br>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div id="fh5co-countdown" style="background:white;">
+					<div class="row animate-box">
+						<div class="col-md-8 col-md-offset-2 text-center heading-section">
+							<h2 style="color:#3d5736;">Save The Date</h2>
+							<!-- <p>Please Fill-up the form to notify you that you're attending. Thanks. -->
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 text-center animate-box">
+							<p class="countdown" style="color:#3d5736;">
+								<span id="days"></span>
+								<span id="hours"></span>
+								<span id="minutes"></span>
+								<span id="seconds"></span>
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div style="background-color: white ;
+				background-repeat: no-repeat;
+				background-position: bottom;
+				background-image: url(images/cover_bg_1.png);
+				background-size:cover;">
+				<div id="fh5co-started"  data-stellar-background-ratio="0.5">
+					<div class="" ></div>
+					<div class="container" >
+						<div class="row animate-box">
+							<div class="col-md-8 col-md-offset-2 text-center heading-section">
+								<h2 style="color:#3d5736;">Buku Tamu</h2>
+								<!-- <p>Please Fill-up the form to notify you that you're attending. Thanks. -->
+							</div>
+						</div>
+						<div class="row animate-box" >
+							<div class="col-md-10 col-md-offset-1">
+								<form class="form-inline">
+									<div class="col-md-6 col-sm-4 col-md-offset-3 col-sm-offset-4">
+										<div id="liveAlertPlaceholder"></div>
+									</div>
+
+									<div class="col-md-6 col-sm-4 col-md-offset-3 col-sm-offset-4">
+										<div class="form-group">
+											<label for="name" class="sr-only">Nama Anda</label>
+											<input type="name" class="form-control" id="nama" placeholder="Nama Anda" style="background:rgba(0, 0, 0, 0.6);">
+										</div>
+									</div>
+									<div class="col-md-6 col-sm-4 col-md-offset-3 col-sm-offset-4">
+										<div class="form-group">
+											<label for="email" class="sr-only">Ucapkan Doa & Selamat</label>
+											<textarea type="text" class="form-control" id="doa" placeholder="Ucapkan Doa & Selamat" style="background:rgba(0, 0, 0, 0.6);"></textarea>
+										</div>
+									</div>
+									<div class="col-md-6 col-sm-4 col-md-offset-3 col-sm-offset-4">
+										<button  type="button" class="btn btn-primary btn-block" onClick="createDoa()" style="background:#3d5736;">KIRIM</button>
+										<br><br>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="fh5co-blog-section" class="fh5co-section-gray">
+				<div class="container">
+					<div class="row row-bottom-padded-md" id="komentar">
+					</div>
+					<div class="col-md-12 text-center">
+						"Terima kasih atas doa dan restunya."<br>( Dwi & Miftah )<br><br>
+						<i>Wassalamu’alaikum Warahmatullahi Wabarakatuh.</i>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+
+	</div>
+
+	<button class="open-button"><div data-video="97XJZYIIBl8" data-autoplay="1" data-loop="1" id="youtube-audio"></div></button>
+
+	<footer>
+		<div id="footer" style="background:#3d5736;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+					</div>
+					<div class="col-md-6 col-md-offset-3 text-center">
+						<p>Made with <i class="icon-heart3"></i>  by <a target="_blank" href="https://www.instagram.com/fasyifaaus/"><i class="icon-instagram"></i> fasyifaaus </a></a> <br><small>Rustic floral vector created by mariadetarosarinda @ www.freepik.com</small></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js"></script>
+	<script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-database.js"></script>
+	<script src="dist/mss.js"></script>
+
+
+
+<script src="https://www.youtube.com/iframe_api"></script>
+<script>
+
+// $( document ).ready(function() {
+
+
+function onYouTubeIframeAPIReady() {
+    var e = document.getElementById("youtube-audio"),
+        t = document.createElement("img");
+    t.setAttribute("id", "youtube-icon"),
+    t.style.cssText = "cursor:pointer;cursor:hand",
+    e.appendChild(t);
+    var a = document.createElement("div");
+    a.setAttribute("id", "youtube-player"),
+    e.appendChild(a);
+    var o = function (e) {
+        var a = e
+            ? "mute.png"
+            : "volume.png";
+        t.setAttribute("src", "../images/" + a)
+    };
+    e.onclick = function () {
+        r.getPlayerState() === YT.PlayerState.PLAYING || r.getPlayerState() === YT.PlayerState.BUFFERING
+            ? (r.pauseVideo(), o(!1))
+            : (r.playVideo(), o(!0))
+    };
+    var r = new YT.Player("youtube-player", {
+        height: "0",
+        width: "0",
+        videoId: e.dataset.video,
+        playerVars: {
+            autoplay: e.dataset.autoplay,
+            loop: e.dataset.loop
+        },
+        events: {
+            onReady: function (e) {
+                r.setPlaybackQuality("small"),
+                o(r.getPlayerState() !== YT.PlayerState.CUED)
+            },
+            onStateChange: function (e) {
+                e.data === YT.PlayerState.ENDED && o(!1)
+            }
+        }
+    })
+}
+
+// })
+
+</script>
+
+
+<!-- SILAKAN JIKA ADA YANG COPAS SCRIPTNYA. BOLEH AJA GRATIS -->
+
+</body>
+</html>
